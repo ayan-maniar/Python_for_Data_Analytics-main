5f547d58d5eb43df1c1c62652ccf12b0b7ed0b62
ayan-maniar <76252713+ayan-maniar@users.noreply.github.com>
2024-08-01 19:10:07 -0400

    Committing From VS

---
+++ 00_Python_Coding/01_Python_Advance/06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -0,0 +1,9 @@
+from MyMainPackage.some_main_script import report_main
+from MyMainPackage.SubPackage import mysubscript
+import mymodule
+
+report_main()
+
+mysubscript.sub_report()
+
+mymodule.my_func()
